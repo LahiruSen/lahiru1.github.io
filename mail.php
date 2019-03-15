338081bff50da46a8250960d845ef7974c9c5d9d
Lahiru Senevirathne <lahiru.16@cse.mrt.ac.lk>
2019-03-15 17:13:35 +0530

    contact me action edited

--- mail.php
+++ mail.php
@@ -6,7 +6,7 @@ $surName = $_POST['surname'];
 $message = $_POST['message'];
 $formcontent=" From: $name \n Phone: $phone \n Message: $message";
 $recipient = "lahiru.16@cse.mrt.ac.lk";
-$subject = "Contact-LahiruSenevirathne.me";
+$subject = "Contact request from personal website";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
 echo "Thank You!" . " -" . "<a href='index.html'> Return Home</a>";
