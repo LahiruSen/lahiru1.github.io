a5f008c17634b25c276c823db87a1c17bfcae3b5
Lahiru Senevirathne <lahiru.16@cse.mrt.ac.lk>
2019-03-15 16:45:01 +0530

    Update mail.php

--- mail.php
+++ mail.php
@@ -9,6 +9,6 @@ $recipient = "lahiru.16@cse.mrt.ac.lk";
 $subject = "Contact-LahiruSenevirathne.me";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-echo "Thank You!" . " -" . "<a href='index.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
+echo "Thank You!" . " -" . "<a href='index.html'> Return Home</a>";
 
 ?>
